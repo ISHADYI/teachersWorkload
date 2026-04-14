0903f9ef65c5accdecad84d1b686b1cc8b5dc325
Shady <shadyworkq@gmail.com>
2026-04-14 11:28:46 +0300

    Мини изменение

--- teachersWorkload/Models/Subject.cs
+++ teachersWorkload/Models/Subject.cs
@@ -14,9 +14,10 @@ namespace teachersWorkload.Models
         public int Hours { get; set; }
 
         [RegularExpression(@"^[1-4][0-9]$", ErrorMessage = "двузначное число: первая цифра от 1 до 4")]
+        // Алана Казбековна, я это всё знаю, честно причестно <3
         public string GroupNumber { get; set; }
 
-        [Range(1, 8, ErrorMessage = "Семестр: от 1 до 8")]
+        [Range(1, 8, ErrorMessage = "Семестр: от 1 до 8")] 
         public int Semester { get; set; }
         public bool IsOnline { get; set; }
     }
